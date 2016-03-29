e7e8ec40899e697ff2b6e3a18406e0c14b64f07e
spacerockzero <janderson@familysearch.org>
2016-03-29 12:25:26 -0600

    works great as long as the css is one line

--- index.js
+++ index.js
@@ -9,13 +9,12 @@ var through = require('through2');
 module.exports = function injectCSS() {
   var stream = through.obj(function(file, enc, cb) {
     
-    var template = 'function injectStyles(rule) {'+
-      '// Works in IE6'+
-      'var div = document.createElement("div");'+
-      'div.innerHTML = "&shy;<style>" + rule + "</style>";'+
-      'document.body.appendChild(div.childNodes[1]);'+
-    '}'+
-    'injectStyles('+file.contents+')';
+    var template = 'function injectStyles(rule) {\n'+
+      'var div = document.createElement("div");\n'+
+      'div.innerHTML = "&shy;<style>" + rule + "</style>";\n'+
+      'document.body.appendChild(div.childNodes[1]);\n'+
+    '}\n'+
+    "injectStyles('"+file.contents+"');\n";
     
     file.contents = new Buffer(template);
     this.push(file);
